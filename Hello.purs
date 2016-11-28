01ec37c14bd2caa6ae5b160b185083290b46d9bc
Steven Taschuk <steven@amotlpaa.org>
2016-11-28 00:38:42 -0500

    Add type declaration for main.

--- Hello.purs
+++ Hello.purs
@@ -1,5 +1,8 @@
 module Hello where
 
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude (Unit)
 
+main :: Eff ( console :: CONSOLE ) Unit
 main = log "Hello, world!"
